681c59a2bb0e21d47f1545702bb4547d6297b8d1
cuongle2444 <cuongle240704@gmail.com>
2025-10-30 00:04:07 +0700

    fix github action

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/nextjs_portfolio" : "",
   output: "export",
   distDir: "dist",
 };
